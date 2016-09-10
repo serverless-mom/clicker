1d41a0b93135cee03eb8cde4a7ab85a20f282048
Toby Fee <toby.fee@nwea.org>
2016-09-09 19:47:21 -0700

    preliminary DB access

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     cool = require('cool-ascii-faces'),
-    chart = require('./lib/chart');
+    chart = require('./lib/chart'),
+    pg = require('pg');
 
 var app = express();
 
@@ -20,6 +21,18 @@ app.get('/cool', (request,response) => {
   response.send(cool())
 });
 
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
+
 app.get('/steve', (request,response) => {
     var data = [1, 2, 12, 12, 12, 21, 21, 39];
     dataChart = chart(data, {
@@ -27,7 +40,7 @@ app.get('/steve', (request,response) => {
         negativePointChar: '░',
         padding: 4
     });
-    response.send( dataChart.replace(/(?:\r\n|\r|\n)/g, '<br />')) //very hacky newline cludge. Do this differently
+    response.send( dataChart.replace(/(?:\r\n|\r|\n)/g, '<br />')); //very hacky newline cludge. Do this differently
 
 });
 
